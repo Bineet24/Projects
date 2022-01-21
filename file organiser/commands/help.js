1bdf5cd3df5786f9de19ca9139aea33633cc798b
Bineet24 <86036476+Bineet24@users.noreply.github.com>
2022-01-21 17:12:44 +0530

    Add files via upload

---
+++ file organiser/commands/help.js
@@ -0,0 +1,9 @@
+function helpFn(){
+    console.log(`list all the commands-
+    1)Tree-node fo.js tree <dir name>
+    2)organize-node fo.js organize <dir name>
+    3)help-node fo.js help`)
+}
+module.exports={
+    helpFnKey : helpFn
+   }
